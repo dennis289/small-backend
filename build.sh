82c52763ff6fab6799eda1b4b75930d5166c7958
dennis <edenismacharia7@gmail.com>
2026-06-02 14:23:56 +0300

    Cors error correction

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
-python manage.py loaddata data.json || true
+# python manage.py loaddata data.json || true
+python manage.py loaddata data.json
